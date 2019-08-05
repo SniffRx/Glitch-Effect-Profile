16613d98b635cf44677e30e03d5605e5e6cc09c4
Fortik <37187657+fortik@users.noreply.github.com>
2019-08-05 16:25:41 +0300

    Added files
    Add project

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" >
+	<head>
+		<meta charset="UTF-8">
+		<title>Glitch Effect (Profile)</title>
+		<link rel="stylesheet" href="css/1style.css">
+		<link rel="preload" type="text/css" as="style" href="css/main.css" onload="this.rel='stylesheet'">
+		<!--link rel="stylesheet" href="css/1style.css"-->
+	</head>
+	<body>
+		<!--h1><span class="glitch" data-text="FORTIKPLAY">FORTIKPLAY</span></h1-->
+		<div class="loading">
+			<div id="wrapper">
+				<div id="main">
+					<header id="header">
+							<h1><font class="unselectable"><span class="glitch" data-text="FORTIKPLAY">FORTIKPLAY</span></font></h1>
+							<!--h2><font size="2">#4573</font></h2-->
+							<nav>
+								<div class="e" style="color:#fff;border:0px solid #0;padding:5px 10px;">
+									<p>
+										<a href="https://discord.gg/RuHhKxQ" arget="_blank">discord | </a>
+										<a href="https://steamcommunity.com/id/FortikPlay/" arget="_blank">steam | </a>
+										<a href="https://www.youtube.com/channel/UCSpyvhCK1f4b90yD9YKJxlQ" target="_blank">youtube | </a>
+										<a href="/404">cheats </a>
+									</p>
+								</div>
+							</nav>
+					</header>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
